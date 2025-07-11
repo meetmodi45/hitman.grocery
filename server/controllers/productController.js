e477dff7f8dcd1ec4b42a1000509a2cb00a812e8
Meet Modi <meetmmodi45@gmail.com>
2025-07-12 02:23:07 +0530

    deploy6

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,7 +3,7 @@ import Product from "../models/Product.js";
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find().sort({ createdAt: -1 });
-    console.log("success");
+    console.log(res.data);
     res.status(200).json({ products });
   } catch (err) {
     console.error("Error fetching products:", err);
